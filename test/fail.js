0c77c1d3c8bf31f3b2f44e07a4488541de0c4b6f
mkwtys <mkwtys@gmail.com>
2024-12-16 10:50:46 +0900

    fix test

--- test/fail.js
+++ test/fail.js
@@ -1,12 +1,11 @@
-'use strict';
-const assert = require('power-assert');
-const backtick = require('../lib/');
+const assert = require("assert");
+const backtick = require("../lib/");
 
-describe('fail', function() {
-  it('glob no match', function() {
-    return backtick('./test/__NO_MATCH__').catch((e) => {
+describe("fail", function () {
+  it("glob no match", function () {
+    return backtick("test/__NO_MATCH__").catch((e) => {
       assert(e instanceof Error);
-      assert(e.message === 'no match template file');
+      assert(e.message === "no match template file");
     });
   });
 });
